b8b7c5ca8b8334a7a847a08522e79a0c75bebb6a
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-04-05 09:12:50 -0400

    cash

--- app/models/admin/cash.rb
+++ app/models/admin/cash.rb
@@ -1,2 +1,17 @@
 class Admin::Cash < ApplicationRecord
+    def category_name
+        if self.category == 1
+           "Российская валюта"
+         else
+         if self.category == 2
+             "Американская валюта"
+        else
+        if self.category == 3
+            "Критовалюта"
+        else
+            "Не задан"
+        end
+        end
+        end
+    end
 end
