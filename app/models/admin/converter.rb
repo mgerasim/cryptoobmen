e8cc453fba22eb324309b5c8d23c8c57f3c7bb5b
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-04-16 04:29:15 -0400

    error_converter

--- app/models/admin/converter.rb
+++ app/models/admin/converter.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'addressable/uri'
+
 class Admin::Converter < ApplicationRecord
   belongs_to :cash_a, class_name: "Admin::Cash"
   belongs_to :cash_b, class_name: "Admin::Cash"
