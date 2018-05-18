f5238f7f72e973d8f8d1b73aeed92bd781b76210
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-05-18 06:56:06 -0400

    #4 Генерация модели заявки на обмен

---
+++ db/migrate/20180518105215_create_obmen.rb
@@ -0,0 +1,18 @@
+class CreateObmen < ActiveRecord::Migration[5.1]
+  def change
+    create_table :obmen do |t|
+      t.float :value_input
+      t.float :value_output
+      t.string :currency_input
+      t.string :currency_output
+      t.string :email
+      t.string :fullname
+      t.string :input_account
+      t.string :output_account
+      t.integer :status
+      t.boolean :agree
+
+      t.timestamps
+    end
+  end
+end
