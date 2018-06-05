057c7b008c85a209ff1c74af82980d0712d2166e
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-06-06 00:12:01 +1000

    Добавить минимальное значание обмена криптовалюты #39

--- app/controllers/cryptocurrencies_controller.rb
+++ app/controllers/cryptocurrencies_controller.rb
@@ -70,6 +70,6 @@ class CryptocurrenciesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cryptocurrency_params
-      params.require(:cryptocurrency).permit(:code, :name, :logo)
+      params.require(:cryptocurrency).permit(:code, :name, :minexchange, :logo)
     end
 end
